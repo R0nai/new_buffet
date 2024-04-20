38d21a79c35b840e01c46dfbcbe743e28a1e64f0
Ronai Melo <amzronai@gmail.com>
2024-04-20 19:53:51 -0300

    Add root route

---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'User visits homepage' do
+  it 'and sees app name' do
+    # Arrange
+
+    # Act
+    visit root_path
+
+    # Assert
+    expect(page).to have_content 'Cadê Buffet'
+  end
+
+end
