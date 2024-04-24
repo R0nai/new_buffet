19487218add7dbea1f8adcfe86b065cfd55c42c5
Ronai Melo <amzronai@gmail.com>
2024-04-24 08:20:36 -0300

    Add owner redirection to new_buffet route while no buffet registered

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :buffet_not_registered_yet
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def buffet_not_registered_yet
+    if current_owner && current_owner.buffet.nil?
+      unless url_for(only_path: true) == destroy_owner_session_path(only_path: true) || url_for(only_path: true) == new_buffet_path || url_for(only_path: true) == buffets_path
+        redirect_to new_buffet_path
+      end
+    end
   end
 end
--- spec/system/owner/owner_signs_in_spec.rb
+++ spec/system/owner/owner_signs_in_spec.rb
@@ -1,9 +1,42 @@
 require 'rails_helper'
 
 describe 'Owner signs-in' do
-  it 'Sucessfully' do
+
+  it 'and gets redirected to new_buffet_path in order to create a buffet' do
     # Arrange
     Owner.create!(name:'Kratos', email: 'abc@de.com', password: 'password')
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'abc@de.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end
+
+    # Assert
+    expect(current_path).to eq new_buffet_path
+
+  end
+
+  it 'Sucessfully' do
+    # Arrange
+    owner = Owner.create!(name:'Kratos', email: 'abc@de.com', password: 'password')
+    buffet = Buffet.create!(brand: 'Kratos Buffet',
+                   corporate_name: 'Kratos Buffet Ltda.',
+                   registration_number: '123456',
+                   phone_number: '12-99999999',
+                   email: 'kratos@uol.com',
+                   address: 'Rua do Kratos, 100',
+                   district: 'Jardim Kratolândia',
+                   city: 'Kratoland',
+                   state: 'SP',
+                   zip_code: '12000-100',
+                   description: 'Pequena descrição',
+                   owner_id: owner.id
+                   )
+
 
     # Act
     visit root_path
@@ -44,4 +77,5 @@ describe 'Owner signs-in' do
     expect(page).not_to have_content 'abc@de.com'
 
   end
+
 end
